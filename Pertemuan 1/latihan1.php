a81f581651ba2bccf44725d05dd1eee2b34ab676
irfanamin24 <irfanamin245@gmail.com>
2021-10-13 16:14:46 +0700

    Kegiatan Pertemuan 2

--- Pertemuan 1/latihan1.php
+++ Pertemuan 1/latihan1.php
@@ -1,7 +1,7 @@
-<form enctype="multipart/form-data" method="post" action="latihan1_upload.php">
+<form enctype="multipart/form-data" method="post" action="latihan1_upload.php"> <!--membuat form dengan metode post dan aksi ke latihan1_upload.php-->
     File yang diupload:
-    <input type="file"name="fupload"><br>
+    <input type="file"name="fupload"><br>   <!--membuat inputan dengan nama fupluad dan bertype file-->
     Deskripsi File: <br>
-    <textarea name="deskripsi" rows="6" cols="20"></textarea><br>
-    <input type=submit value=Upload>
+    <textarea name="deskripsi" rows="6" cols="20"></textarea><br>   <!--membuat inputan berupa textarea dengan penamaan deskripsi-->
+    <input type=submit value=Upload>    <!--membuat submit-->
 </form>
